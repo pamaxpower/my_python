896bf82b7b5ce89328bbc9fd877764f0ad9d01bf
lordik <jetlord@mail.ru>
2022-11-25 14:16:23 +0300

    sem2

--- home_work_1/ex2.py
+++ home_work_1/ex2.py
@@ -4,6 +4,12 @@
 
 time = int(input('Введите время в секундах: '))
 hour = time // 3600
-minute = (time - hour*3600) // 60
-second = time - hour*3600 - minute*60
+minute = (time - hour*3600) // 60               # (time % 3600) // 60
+second = time - hour*3600 - minute*60           # time % 60
 print(f'{hour}:{minute}:{second}') 
+
+
+sec = time
+hou = sec / 3600
+minu = sec / 60
+print(f'Время {hou:.5f} : {minu} : {sec}')
