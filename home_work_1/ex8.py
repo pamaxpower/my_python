5f69fc764532246893851e7d4c614305e15ca19e
lordik <jetlord@mail.ru>
2022-12-01 14:53:06 +0300

    123

--- home_work_1/ex8.py
+++ home_work_1/ex8.py
@@ -2,16 +2,20 @@
 ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 """
 # ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z - закон Де Моргана
-# ¬(X ⋁ Y ⋁ Z) по-другому чстается как not(X and Y and Z)
-# ¬X ⋀ ¬Y ⋀ ¬Z читается как (not X) or (not Y) or (not Z)
+# ¬(X ⋁ Y ⋁ Z) по-другому читается как not(X or Y or Z)
+# ¬X ⋀ ¬Y ⋀ ¬Z читается как (not X) and (not Y) and (not Z)
 
 predicate = [True, False]
 for x in predicate:
     for y in predicate:
         for z in predicate:
-            left = not (x and y and z)
-            rigth = (not x) or (not y) or (not z)
+            left = not (x or y or z)
+            rigth = (not x) and (not y) and (not z)
             if left == rigth:
                 print(f'Для {x,y,z} равенство верное')
             else:
                 print(f'Для {x,y,z} равенство неверное')
+
+
+# ⋁ - дизъюнкция (операция ИЛИ) = or
+# ⋀ - конъюнкция (логическое И) = and
